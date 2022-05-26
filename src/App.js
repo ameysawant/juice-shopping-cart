4b920f298b526d7f9a31da6c13420099f8fb1cee
unknown <mailforamey@gmail.com>
2022-05-26 17:37:01 +0530

    home laptop

--- src/App.js
+++ src/App.js
@@ -6,57 +6,6 @@ import Homepage from "./components/homepage/Index";
 const App = () => {
   return (
     <>
-      <h1 className="heading-lg">heading</h1>
-      <h1 className="heading-md">heading</h1>
-      <h1 className="heading-sm">heading</h1>
-      <button className="btn">button</button>
-      <button className="btn btn-black">button</button>
-      <button className="btn btn-disabled">button</button>
-      <table>
-        <tr>
-          <th>tesing</th>
-          <th>tesing</th>
-          <th>tesing</th>
-          <th>tesing</th>
-        </tr>
-        <tr>
-          <td>one</td>
-          <td>one</td>
-          <td>one</td>
-          <td>one</td>
-        </tr>
-        <tr>
-          <td>one</td>
-          <td>one</td>
-          <td>one</td>
-          <td>one</td>
-        </tr>
-        <tr>
-          <td>one</td>
-          <td>one</td>
-          <td>one</td>
-          <td>one</td>
-        </tr>
-        <tr>
-          <td>one</td>
-          <td>one</td>
-          <td>one</td>
-          <td>one</td>
-        </tr>
-        <tr>
-          <td>one</td>
-          <td>one</td>
-          <td>one</td>
-          <td>one</td>
-        </tr>
-      </table>
-      <input type="text" className="f-control" />
-      <select name="" id="" className="f-control">
-        <option value="">select</option>
-        <option value="">one</option>
-        <option value="">2</option>
-        <option value="">3</option>
-      </select>
       <Header />
       <Homepage />
       <Footer />
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,49 @@
 import React from "react";
+import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <div className="dvHeader">
+        <div className="container-lg">
+          <div className="row align-items-center pt-1">
+            <div className="col d-lg-none">
+              <i class="fa-solid fa-bars"></i>
+            </div>
+            <div className="col col-lg-1 text-center text-lg-left">
+              <img
+                width={50}
+                src="https://static.wixstatic.com/media/2c0034_a27b95faba1d432fbddcf6ac4e9683ba~mv2.png"
+                alt=""
+              />
+            </div>
+            <div className="dvSlideMenu col-lg-10 px-0 px-lg-3">
+              <button className="btn btn-black closeBtn d-lg-none">
+                <i class="fa-solid fa-xmark"></i>
+              </button>
+              <ul className="dvMenu">
+                <li>
+                  <a href="">Home</a>
+                </li>
+                <li>
+                  <a href="">Shop</a>
+                </li>
+                <li>
+                  <a href="">Login</a>
+                </li>
+                <li>
+                  <a href="">Signup</a>
+                </li>
+              </ul>
+            </div>
+            <div className="col col-lg-1 text-right">
+              <i class="fa-solid fa-cart-shopping"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
